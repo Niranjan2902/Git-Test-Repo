b25b98db055564d5f2c5a497935a0974c35a9e3f
Niranjan <niranjankhavale2498@gmail.com>
2025-03-28 16:53:51 +0530

    add new cpp code

--- git.cpp
+++ git.cpp
@@ -1,6 +1,9 @@
 #include<iostream>
 using namespace std;
 int main(){
+    int a=10;
+    int b=20;
     cout<<"This is my git and github task."<<endl;
-    cout<<"this is for linking up both for push pull command practice";
+    cout<<"this is for linking up both for push pull command practice"<<endl;
+    cout<<"new updates in code "<<a+b;
 }
